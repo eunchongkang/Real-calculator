049806e375be77ac1dbc099bcc3570f8a0c798c5
Your Name <you@example.com>
2024-07-30 17:24:56 +0900

    2-2 요구사항 Exception 제외

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,57 +8,60 @@ public class App {
 
     public static void main(String[] args) {
 
+        Calculator calculator = new Calculator(); //Calculator 객체 생성
+
         Scanner sc = new Scanner(System.in);
 
 
-        // 값을 무한히 저장하기 위해 동적 배열인 ArrayList 사용
+        // 값을 무한히 저장하기 위해 동적 배열인 ArrayList 사용을 위해 선언 및 생성 하였음
         ArrayList<Integer> results = new ArrayList<>();
 
 
         //반복을 위해 while () {} 사용 if문에 break를 넣어 exit 할때 반복중지
-        int result;
+        int result = 0;
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt();
             System.out.print("두 번째 숫자를 입력하세요: ");
             int num2 = sc.nextInt();
             System.out.print("사칙연산 기호를 입력하세요: "); //+,-,*,/
-            char operator1 = sc.next().charAt(0);
+            String operator = sc.next();
             // sc.next 변수뒤 charAt 메서드를 적용하여 인덱스 번호에 있는 문자를  char 로 변환
 
-            result = 0;
 
 // if 조건문을 사용하여 사칙연산 기호에 따라 연산을 하였고
 // 사칙연산의 경우가 여러개 여서 else if 중첩문을 사용하였다
-            if (operator1 == '+') {
-                result = num1 + num2;
-            } else if (operator1 == '-') {
-                result = num1 - num2;
-            } else if (operator1 == '*') {
-                result = num1 * num2;
-            } else if (operator1 == '/') {
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    continue;
-                }
-            } else {
-                System.out.println("사칙연산 기호를 올바르게 입력해주세요 (+,-,*,/) ");
-            }
+//            if (operator1 == '+') {
+//                result = num1 + num2;
+//            } else if (operator1 == '-') {
+//                result = num1 - num2;
+//            } else if (operator1 == '*') {
+//                result = num1 * num2;
+//            } else if (operator1 == '/') {
+//                if (num2 != 0) {
+//                    result = num1 / num2;
+//                } else {
+//                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+//                    continue;
+//                }
+//            } else {
+//                System.out.println("사칙연산 기호를 올바르게 입력해주세요 (+,-,*,/) ");
+//            }
+//
+//
+//            results.add(result); // results 에 연산결과 값을 추가
+//
+//            System.out.println("결과: " + result);
+//
+//
+//            System.out.println("저장결과:");
+//            for (int rst : results) {
+//                System.out.println(rst);   // for 문을 통해 연산결과를 rst 변수명으로 지정후 프린트
+//            }
 
+            calculator.calculate(operator, num1, num2);
+            //calculator 클래스의 calculate 매서드 호출, 연산실행
 
-            results.add(result); // results 에 연산결과 값을 추가
-
-            System.out.println("결과: " + result);
-
-
-            // exit 의 문자열을 작성하기 위해 String을 사용
-
-            System.out.println("저장결과:");
-            for (int rst : results) {
-                System.out.println(rst);   // for 문을 통해 연산결과를 rst 변수명으로 지정후 프린트
-            }
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             String removeChat = sc.next();
@@ -67,15 +70,15 @@ public class App {
             } // remove를 타이핑하고 결과값이 비어있지 않은경우 첫번째 요소를 삭제
 
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            String operator2 = sc.next();
-            if (operator2.equals("inquiry")) {
-                for (int rst : results) {
+            String inquiry = sc.next();
+            if (inquiry.equals("inquiry")) {
+                for (int rst : results) {   //향상된 for문을 사용하여 저장결과값 rst를 조회 할 수 있도록 함
                     System.out.println(rst);
                 }
             }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) 다시 시작하시려면 타이핑 해주세요");
-            String operator = sc.next();
+            operator = sc.next();
             if (operator.equals("exit")) { //equals 메서드를 사용해 두 문자열의 동일한 값을 가짐을 확인
                 System.out.println("프로그램을 종료합니다.");
                 break;
